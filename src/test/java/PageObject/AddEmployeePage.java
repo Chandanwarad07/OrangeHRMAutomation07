e1d240442f76ac7676d43bf726535ebfda9b5a19
Chandan Kumar <chandanwarad07@gmail.com>
2026-06-29 09:25:45 +0530

    initial Commit

--- src/test/java/PageObject/AddEmployeePage.java
+++ src/test/java/PageObject/AddEmployeePage.java
@@ -1,8 +1,14 @@
 package PageObject;
 
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class AddEmployeePage extends BasePage {
 
@@ -23,18 +29,36 @@ public class AddEmployeePage extends BasePage {
     WebElement btnSave;
 
     public void enterFirstName(String fname) {
+        txtFirstName.clear();
         txtFirstName.sendKeys(fname);
     }
 
     public void enterMiddleName(String mname) {
+        txtMiddleName.clear();
         txtMiddleName.sendKeys(mname);
     }
 
     public void enterLastName(String lname) {
+        txtLastName.clear();
         txtLastName.sendKeys(lname);
     }
 
     public void clickSave() {
-        btnSave.click();
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+
+        // Wait for OrangeHRM loading overlay to disappear
+        wait.until(ExpectedConditions.invisibilityOfElementLocated(
+                By.cssSelector(".oxd-form-loader")));
+
+        // Wait until Save button is clickable
+        wait.until(ExpectedConditions.elementToBeClickable(btnSave));
+
+        try {
+            btnSave.click();
+        } catch (Exception e) {
+            JavascriptExecutor js = (JavascriptExecutor) driver;
+            js.executeScript("arguments[0].click();", btnSave);
+        }
     }
 }
--- src/test/java/utilities/ExtentManager.java
+++ src/test/java/utilities/ExtentManager.java
@@ -1,5 +1,30 @@
 package utilities;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+
 public class ExtentManager {
 
+    private static ExtentReports extent;
+
+    public static ExtentReports getInstance() {
+
+        if (extent == null) {
+
+            ExtentSparkReporter spark =
+                    new ExtentSparkReporter("reports/ExtentReport.html");
+
+            spark.config().setReportName("OrangeHRM Automation Report");
+            spark.config().setDocumentTitle("Automation Test Report");
+
+            extent = new ExtentReports();
+            extent.attachReporter(spark);
+
+            extent.setSystemInfo("Tester", "Chandan Kumar");
+            extent.setSystemInfo("Project", "OrangeHRM Automation");
+            extent.setSystemInfo("Browser", "Chrome");
+        }
+
+        return extent;
+    }
 }
